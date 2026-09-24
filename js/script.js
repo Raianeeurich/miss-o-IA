3d33abf67ba4b017d8b6755df47d5723c20a1692
Raianeeurich <mittlstaedt.raiane@escola.pr.gov.br>
2026-09-24 10:12:27 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+import { perguntas } from "./perguntas.js";
+
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
